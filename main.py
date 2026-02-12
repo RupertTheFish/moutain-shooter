d36b93377ed54b584e21768bd2a266b632aeb9f1
RafaelKruger <kruger.rafael@outlook.com>
2026-02-12 19:45:25 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+print('Setup start')
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((800, 600))
+print('Setup end')
+
+print('Loop start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() # End Pygame
