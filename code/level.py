5486905755d82c6f3085b296d5bd0e1f4b7ec425
RafaelKruger <kruger.rafael@outlook.com>
2026-03-20 17:11:17 -0300

    Adding classes generated from StarUML

---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.name = None
+        self.window = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
